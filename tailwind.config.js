c88c6ddbcb0d20c2b29e1234338bf7a448f5c6d7
Diego AZ <21963801+slinky97@users.noreply.github.com>
2024-03-14 23:04:43 -0600

    Header v0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#1e281e",
+      },
+      boxShadow: {
+        "solid-black": "0 0 0 4px rgba(0, 0, 0, 0.65)",
+      },
+    },
   },
   variants: {
     extend: {},
