5cb011d67e4ae626125318a34840819a4397a316
Diego AZ <21963801+slinky97@users.noreply.github.com>
2024-03-15 18:20:37 -0600

    Header v0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
     extend: {
       colors: {
         "dark-gray": "#1e281e",
+        "sky-blue": "#37d1fb",
+        "pink-rose": "#ee4bb4",
       },
       boxShadow: {
         "solid-black": "0 0 0 4px rgba(0, 0, 0, 0.65)",
+      },
+      backgroundImage: {
+        "header-texture": "url('/public/img/hero-header-layer-1-large-up.jpg')",
       },
     },
   },
