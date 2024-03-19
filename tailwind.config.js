7aa210415df9c94f158c27016fba14cfa9e38c0c
Diego AZ <21963801+slinky97@users.noreply.github.com>
2024-03-19 01:45:15 -0600

    Vista mobile / header version 1.0 / Fondo mobile
    Se ajusto de mejor manera el cotenido del arte inicial para que tuviera mejor rendimiento.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,12 @@ module.exports = {
         "dark-gray": "#1e281e",
         "sky-blue": "#37d1fb",
         "pink-rose": "#ee4bb4",
+        "red-nintendo": "#e60012",
       },
       boxShadow: {
         "solid-black": "0 0 0 4px rgba(0, 0, 0, 0.65)",
       },
-      backgroundImage: {
-        "header-texture": "url('/public/img/hero-header-layer-1-large-up.jpg')",
-      },
+      backgroundImage: {},
     },
   },
   variants: {
